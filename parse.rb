8aee075c55b61090fcce36d7ab8a91077bafc905
Dwayne Macgowan <dwaynemac@gmail.com>
2010-05-20 17:38:08 -0300

    remove RubyMine files, added them to .gitignore, include default teacher in .csv

--- parse.rb
+++ parse.rb
@@ -17,12 +17,17 @@ FasterCSV.open("caja_chica.csv","w") do |csv|
   (2...wb.sheet_count).each do |sn|
     ws = wb.worksheet(sn)
     date = ws.row(0).at(1).date
+    default_teacher = ws.row(1).at(1).to_s
 
     # iterate through data box in work sheet
     (4...74).each do |n|
       row = ws.row(n)
 
+
       teacher = row.at(3).to_s # teacher
+      if teacher.empty?
+        teacher = default_teacher
+      end
       desc    = row.at(4).to_s # description
       inc     = row.at(5).to_f # income
       out     = row.at(6).to_f # outcome
